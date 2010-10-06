98b88e29a879b44f565213a5c96a4524d1809ac3
Gerrit Wanderer <wanderwelten@googlemail.com>
2010-10-06 18:04:45 +0200

    Add some more Mail-Config changes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,10 +3,10 @@
 Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in DeviseMailer.
-  config.mailer_sender = "please-change-me@config-initializers-devise.com"
+  config.mailer_sender = "gw@wildner-designer.de"
 
   # Configure the class responsible to send e-mails.
-  # config.mailer = "Devise::Mailer"
+  config.mailer = "Devise::Mailer"
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
