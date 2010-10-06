8d7283776a43e924974fea3e5095ff94d10b9c9e
Gerrit Wanderer <wanderwelten@googlemail.com>
2010-10-06 15:29:25 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CheatsheetCmsRails::Application.config.secret_token = '6faad5a758ad6b53ebd26048f11d8d63686a33d3f21c02c184da691c3c495e6af6b70451c0c4afb35287798164d279a2960fe82f6f8dd34e3d17f4baf984cf5c'
