060ef92339f803f8a309c2f9083a3f3a0213fd6c
Gerrit Wanderer <wanderwelten@googlemail.com>
2010-10-25 17:13:41 +0200

    Write SMTP-Settings with environment-vars or direct input

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,15 @@
+# !!! WATCH OUT !!!
+# The sensitive data for SMTP-Access are stored as config vars at heroku (http://docs.heroku.com/config-vars)
+# execute the following command with the smtp-data from your terminal
+# user$ heroku config:add SMTP_SERVER=mail.example.org SMTP_DOMAIN=example.org SMTP_USER=exampleuser SMTP_PASSWORD=examplepassword
+# or change the additional direct input
+
 ActionMailer::Base.smtp_settings = {  
-  :address              => "mail.example.org",  
+  :address              => ENV['SMTP_SERVER'] || 'mail.example.org',
   :port                 => 25,  
-  :domain               => "example.org",  
-  :user_name            => "user",  
-  :password             => "password",  
+  :domain               => ENV['SMTP_DOMAIN'] || 'example.org',
+  :user_name            => ENV['SMTP_USER'] || 'exampleuser',
+  :password             => ENV['SMTP_PASSWORD'] || 'examplepassword',
   :authentication       => "plain",  
   :enable_starttls_auto => false  
 }
